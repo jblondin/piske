d7e0709baf9c65cceea5ce6efdda634b32e56346
Jamie Blondin <jblondin@gmail.com>
2017-12-02 17:28:12 -0500

    Allowed transpiler to overwrite files in existing project directory

--- transpiler/src/main.rs
+++ transpiler/src/main.rs
@@ -1,16 +1,21 @@
 extern crate cargo;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::io::{Read, Write};
-use std::fs::{OpenOptions, File};
+use std::fs::File;
 
 extern crate piske;
 
 fn create_project_dir(proj_dir: &str) -> PathBuf {
-    let new_opts = cargo::ops::NewOptions::new(None, true, false, proj_dir, None);
     let config = cargo::util::Config::default().unwrap();
-    cargo::ops::new(&new_opts, &config).unwrap();
-    config.cwd().join(new_opts.path)
+    let new_opts = cargo::ops::NewOptions::new(None, true, false, proj_dir, None);
+    let target_path = config.cwd().join(new_opts.path);
+    if Path::new(proj_dir).is_dir() {
+        println!("Using existing project directory {}", target_path.to_str().unwrap());
+    } else {
+        cargo::ops::new(&new_opts, &config).unwrap();
+    }
+    target_path
 }
 
 macro_rules! try_file {
@@ -44,6 +49,7 @@ macro_rules! try_file {
 }
 
 fn transpile_file(file_name: &str, proj_dir: &str) {
+    let proj_name = Path::new(proj_dir).file_name().unwrap().to_str().unwrap();
     let proj_dir = create_project_dir(proj_dir);
 
     let transpiled = try_file!(File::open(file_name), mut file, {
@@ -66,11 +72,19 @@ fn transpile_file(file_name: &str, proj_dir: &str) {
     });
 
     // write Cargo.toml
-    try_file!(OpenOptions::new().append(true).open(proj_dir.join("Cargo.toml")), mut f, {
-        try_file!(f.write_all(r#"
-psk_std = { path = "../piske/psk_std" }
+    try_file!(File::create(proj_dir.join("Cargo.toml")), mut f, {
+        let cargo_contents = format!(
+r#"[package]
+name = "{}"
+version = "0.1.0"
+authors = ["Jamie Blondin <jblondin@gmail.com>"]
+
+[dependencies]
+psk_std = {{ path = "../piske/psk_std" }}
 image = "*"
-        "#.as_bytes()), {});
+"#, proj_name);
+
+        try_file!(f.write_all(cargo_contents.as_bytes()), {});
     });
 
 }
